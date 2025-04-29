07a1d4cebecd823aeaaf30e2c0a5997223baf94e
RobinGuyton <gladiatorrobin@aol.com>
2025-04-28 18:32:43 -0700

    Add project files.

---
+++ COMP003B.LectureActivity5/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureActivity5.Models
+{
+    public class Course
+    {
+        public int StudentId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public virtual ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
